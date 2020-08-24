520c74153fa543f9d22af455fe26b6d7a82bdb11
CL-75 <49925072+CL-75@users.noreply.github.com>
2020-08-24 11:29:09 -0400

    Add files via upload

---
+++ JS/basic_javascript_3.js
@@ -0,0 +1,37 @@
+function myFunctionAdd() {
+    var myMath = 3 + 5;
+    document.getElementById("MathAdd").innerHTML = "3 + 5 = " + myMath;
+}
+
+function myFunctionSub() {
+    var myMath = 5 - 3;
+    document.getElementById("MathSub").innerHTML = "5 - 3 = " + myMath;
+}
+
+function myFunctionMult() {
+    var myMath = 3 * 5;
+    document.getElementById("MathMult").innerHTML = "3 * 5 = " + myMath;
+}
+
+function myFunctionMod() {
+    var myMath = 5 % 3;
+    document.getElementById("MathMod").innerHTML = "5 / 3 = " + myMath;
+}
+
+function myFunctionInc() {
+    var myMath = 5;
+    myMath++;
+    document.getElementById("MathInc").innerHTML = "5 increased by one is " + myMath;
+}
+
+function myFunctionDec() {
+    var myMath = 5;
+    myMath--;
+    document.getElementById("MathDec").innerHTML = "5 decreased by one is " + myMath;
+}
+
+function myFunctionRandom() {
+    var rando = Math.floor(Math.random() * 100);
+    document.getElementById("MathRand").innerHTML = "Your random number is " + rando;
+
+}
