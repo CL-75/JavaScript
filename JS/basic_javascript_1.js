8526bb8c1dc065cdf7054acdfe70bce40569b960
CL-75 <49925072+CL-75@users.noreply.github.com>
2020-08-24 10:04:35 -0400

    Create basic_javascript_1.js

---
+++ JS/basic_javascript_1.js
@@ -0,0 +1,12 @@
+window.alert("Look behind you!");   // Creating an alert box with a message
+
+var A = "I am feeling ";
+var B = "very good today.";
+document.write(A + B);            // Assigning string values to two different variables and concatenating them
+
+var taste = "Brussel sprouts are gross.";
+var taste = taste.fontcolor("green");
+document.write(taste);          // Assigning a string value to a variable and printing that text in a different color
+
+
+document.write(3 + 3);     // Printing an expression 
