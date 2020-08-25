8575490f5f13ca9fa1133375d735dd808f797cf0
CL-75 <49925072+CL-75@users.noreply.github.com>
2020-08-25 14:48:01 -0400

    Add files via upload

---
+++ JS/basic_javascript_5.js
@@ -0,0 +1,19 @@
+document.write(typeof "hello");
+
+document.write("50" + 2);
+
+document.write(5 == 5);
+
+var x = 4;
+var y = "4";
+document.write(x === y);
+
+document.write(10 > 2);
+
+document.write(10 < 2);
+
+document.write(10 > 2 && 9 > 5);
+
+document.write(10 > 2 || 3 > 5);
+
+document.write(10 != 2);
