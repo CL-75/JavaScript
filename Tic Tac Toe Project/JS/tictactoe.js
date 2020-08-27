073dced06976d64b31dfac84665a16f8d28dd3bf
CL-75 <49925072+CL-75@users.noreply.github.com>
2020-08-26 22:19:13 -0400

    Create tictactoe.js

---
+++ Tic Tac Toe Project/JS/tictactoe.js
@@ -0,0 +1,166 @@
+let activePlayer = 'X';               // Keeps track of who's turn it is
+let selectedSquares = [];            // Stores an array of moves, helps to determine win conditions
+
+// For placing an X or O in a square
+function placeXOrO(squareNumber) {
+    if (!selectedSquares.some(element => element.includes(squareNumber))) {
+
+        let select = document.getElementById(squareNumber);
+
+        if (activePlayer === 'X') {
+            select.style.backgroundImage = 'url("./Images/x.png")';  //If active player is X, then the x image is used
+        }
+        else {
+           select.style.backgroundImage = 'url("./Images/o.png")'   // if active player is O, then the o image is used
+        }
+   
+        selectedSquares.push(squareNumber + activePlayer);
+        checkWinCondition();
+
+    if (activePlayer === 'X') {
+        activePlayer = 'O';
+    }
+    else {
+       activePlayer = 'X';
+    }
+
+    audio('./Media/place.mp3');    // Plays placement sound when an X or O is placed
+
+    // Checks to see if it is the computer's turn
+    if (activePlayer === 'O') {
+        disableClick();
+        setTimeout(function () { computersTurn(); }, 1000);
+    }
+    return true;
+}
+
+// Random square is selected "by the computer"
+function computersTurn() {
+    let success = false;
+    let pickASquare;
+    while (!success) {
+        pickASquare = String(Math.floor(Math.random() * 9));
+        if (placeXOrO(pickASquare)) {
+            placeXOrO(pickASquare);
+            success = true;
+        };
+    }
+  }
+
+}
+
+// Checks all win conditions
+function checkWinCondition () {
+
+    if (arrayIncludes('0X', '1X', '2X')) {  drawWinLine(50, 100, 558, 100);  }
+
+    else if (arrayIncludes('3X', '4X', '5X')) {  drawWinLine(50, 304, 558, 304);  }
+
+    else if (arrayIncludes('6X', '7X', '8X')) {  drawWinLine(50, 508, 558, 508);  }
+
+    else if (arrayIncludes('0X', '3X', '6X')) {  drawWinLine(100, 50, 100, 558);  }
+
+    else if (arrayIncludes('1X', '4X', '7X')) {  drawWinLine(304, 50, 304, 558);  }
+
+    else if (arrayIncludes('2X', '5X', '8X')) {  drawWinLine(508, 50, 508, 558);  }
+
+    else if (arrayIncludes('6X', '4X', '2X')) {  drawWinLine(100, 508, 510, 90);  }
+
+    else if (arrayIncludes('0X', '4X', '8X')) {  drawWinLine(100, 100, 520, 520);  }
+
+    else if (arrayIncludes('0O', '1O', '2O')) {  drawWinLine(50, 100, 558, 100);  }
+
+    else if (arrayIncludes('3O', '4O', '5O')) {  drawWinLine(50, 304, 558, 304);  }
+
+    else if (arrayIncludes('6O', '7O', '8O')) {  drawWinLine(50, 508, 558, 508);  }
+
+    else if (arrayIncludes('0O', '3O', '6O')) {  drawWinLine(100, 50, 100, 558);  }
+
+    else if (arrayIncludes('1O', '4O', '7O')) {  drawWinLine(304, 50, 304, 558);  }
+
+    else if (arrayIncludes('2O', '5O', '8O')) {  drawWinLine(508, 50, 508, 558);  }
+
+    else if (arrayIncludes('6O', '4O', '2O')) {  drawWinLine(100, 508, 510, 90);  }
+
+    else if (arrayIncludes('0O', '4O', '8O')) {  drawWinLine(100, 100, 520, 520);  }
+
+    else if (selectedSquares.length >= 9) {
+        audio('./Media/tie.mp3');
+        setTimeout(function () {  resetGame();  }, 1000);
+    }
+
+    function arrayIncludes(squareA, squareB, squareC) {
+        const a = selectedSquares.includes(squareA);
+        const b = selectedSquares.includes(squareB);
+        const c = selectedSquares.includes(squareC);
+
+        if (a === true && b === true && c === true) {  return true;  }
+    }
+
+}
+
+// Makes body element temporarily unclickable
+function disableClick() {
+    body.style.pointerEvents = 'none';
+    setTimeout(function() {body.style.pointerEvents = 'auto';}, 1000)
+}
+
+// plays audio at appropriate time
+function audio(audioURL) {
+    let audio = new Audio(audioURL);
+    audio.play();
+}
+
+// Draws a line across the board for the winner
+function drawWinLine(coordX1, coordY1, coordX2, coordY2) {
+    const canvas = document.getElementById("win-lines");
+    const c = canvas.getContext('2d');
+    let x1 = coordX1, y1 = coordY1, x2 = coordX2, y2 = coordY2, x = x1, y = y1;
+
+    function animateLineDrawing() {
+        const animationLoop = requestAnimationFrame(animateLineDrawing);
+        c.clearRect(0, 0, 608, 608);
+        c.beginPath();
+        c.moveTo(x1, y1);
+        c.lineTo(x, y);
+        c.lineWidth = 10;
+        c.strokeStyle = "rgba(70, 255, 33, .8)";
+        c.stroke();
+        
+        if (x1 <= x2 && y1 <= y2) {
+            if (x < x2) {  x += 10;  }
+            if (y < y2) {  y += 10;  }
+            if (x >= x2 && y >= y2) {  cancelAnimationFrame(animationLoop);  }
+        }
+
+        if (x1 <= x2 && y1 >= y2) {
+            if (x < x2) {  x += 10;  }
+            if (y > y2) {  y -= 10;  }
+            if (x >= x2 && y <= y2) {  cancelAnimationFrame(animationLoop); }
+      }
+
+    }
+
+    function clear() {
+        const animationLoop = requestAnimationFrame(clear);
+        c.clearRect(0, 0, 608, 608);
+        cancelAnimationFrame(animationLoop);
+    }
+
+    disableClick();
+    audio('./Media/winGame.mp3');
+    animateLineDrawing();
+    setTimeout(function () {  clear();  resetGame();  }, 1000);
+}
+
+
+
+function resetGame() {
+    for (let i = 0; i < 9; i++) {
+        let square = document.getElementById(String(i));
+        square.style.backgroundImage = '';
+    }
+
+    selectedSquares = [];
+}
+
